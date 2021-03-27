dab0bb1aec4dbb6855c053dfaea743d4cf534902
Collin Delker <ziaplot@collindelker.com>
2021-03-27 16:08:07 -0600

    Version 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name = 'ziaplot',
-    version = '0.1',
+    version = '0.2',
     description = 'Draw light-weight plots, graphs, and charts',
     author = 'Collin J. Delker',
     author_email = 'ziaplot@collindelker.com',
